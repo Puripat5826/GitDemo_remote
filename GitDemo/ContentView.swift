64b4cb33e40a336951fb5b53da03b8cf4e82bccd
Puripat Kitkarncharoensin <appleid0909@icloud.com>
2024-01-30 23:31:22 +0700

    Initial Commit

---
+++ GitDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitDemo
+//
+//  Created by Puripat Kitkarncharoensin on 30/1/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitDemo/GitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoApp.swift
+//  GitDemo
+//
+//  Created by Puripat Kitkarncharoensin on 30/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
